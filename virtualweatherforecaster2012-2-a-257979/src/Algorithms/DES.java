b89c208c0e428e8d99395e8059d2928cda7c6194
Santiago <Santiago@Tk-01>
2012-09-08 13:25:31 -0500

    DES algortihm, Revised.

--- virtualweatherforecaster2012-2-a-257979/src/Algorithms/DES.java
+++ virtualweatherforecaster2012-2-a-257979/src/Algorithms/DES.java
@@ -1,64 +1,33 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Algorithms;
 
-
+//comentario 1 no sirve :D
 public class DES {
     private double lt;
     private double bt;
-    private double ft;
     private double a = 0.2;
     private double g = 0.42 ;
     private double lt1;
     private double bt1;
-    private double ft1;
-    
+    private double input [];
     
     private double predictions[];
     
-    public DES(){}
-    
-    public DES (double input[]){
+    void DES (){
    
-     if(input.length ==1){
-       predictions[1]=input[0];
-     }
-     
-     else
-     {
-        lt= input[0]; // este renglon no es necesario ocupa memoria queda escrito por cuestiones de teoria
-        bt= input[1]-input[0]; // este renglon no es necesario ocupa memoria queda escrito por cuestiones de teoria
-        ft= input[0]; // este renglon no es necesario ocupa memoria queda escrito por cuestiones de teoria
+        lt= input[0]; 
+        bt= input[1]-input[0]; 
+        predictions[1]= input[0]; 
         for(int i=1; i<=input.length; i=i+1)
        {
-        predictions[i]=loop(input[i], input[i+1]);  /*setPredictions para cada prediction[i]*/ 
-         
-       }
-      
-     }
-     
-     
         
-    }
-    
-    double loop (double y1, double y2)
-    {
-        lt1=lt;
-        bt1=bt;
+            lt1 = (a*input[i])+(1-a)*(lt+bt);
+            bt1 = g*(lt1 - lt)+(1-g)*bt;
+            predictions[i] = lt1 + bt1;   
+           
+             lt=lt1;
+             bt=bt1;
             
-            lt = (a*y1)+(1-a)*(lt1+bt1);
-            bt = g*(lt - lt1)+(1-g)*bt1;
-            ft1 = lt + bt;         
-        
-        return ft1;
-    
-    }
-
-
-
-}
-
-
-
+       
+      }
+      
+    }}
