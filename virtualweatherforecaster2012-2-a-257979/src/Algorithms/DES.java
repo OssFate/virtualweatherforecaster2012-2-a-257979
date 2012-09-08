8078bfa2ea4a65963c757b58df71dc1bb76a1619
Oswald <Oswald@USUARIO-PC>
2012-09-08 18:22:59 -0500

    Created the DMA algorithm, waiting for the others 2 to implement Template Method...

---
+++ virtualweatherforecaster2012-2-a-257979/src/Algorithms/DES.java
@@ -0,0 +1,31 @@
+package Algorithms;
+
+public class DES extends weatherAlgorithms{
+    
+    private double lt;
+    private double bt;
+    private double lt1;
+    private double bt1;
+    private double a = 0.2;
+    private double g = 0.42;
+    
+    
+    @Override
+    void logic(){
+        lt = At[0]; 
+        bt = At[1] - At[0]; 
+        Ft[1] = At[0]; 
+        
+        for(int i=1; i < At.length; i++) {
+            
+            this.lt1 = (this.a*this.At[i])+(1- this.a)*(this.lt + this.bt);
+            this.bt1 =  this.g*(this.lt1 - this.lt) + (1 - this.g)*this.bt;
+            
+            this.Ft[i] = this.lt1 + this.bt1;
+            
+            this.lt = this.lt1;
+            this.bt = this.bt1;
+        }
+    }
+    
+}
